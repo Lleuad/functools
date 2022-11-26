781e28fc910bbf314467acb63cb4ecbaf9c43a20
Erik Nonhebel <eriknonhebel@hetnet.nl>
2022-11-26 20:22:42 +0100

    Optimised Map and MaptoStr

--- functools/map.go
+++ functools/map.go
@@ -3,16 +3,18 @@ package functools
 import "fmt"
 
 func Map[T any, V any](f func(T) V, slice []T) (out []V) {
-	for _, v := range slice {
-		out = append(out, f(v))
+	out = make([]V, len(slice))
+	for i, v := range slice {
+		out[i] = f(v)
 	}
 
 	return
 }
 
 func MaptoStr[T any](slice []T) (out []string) {
-	for _, v := range slice {
-		out = append(out, fmt.Sprint(v))
+	out = make([]string, len(slice))
+	for i, v := range slice {
+		out[i] = fmt.Sprint(v)
 	}
 
 	return
